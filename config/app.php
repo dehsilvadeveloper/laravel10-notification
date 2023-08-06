75c60d184bf79f6579ce57437a6b087c4aec67b2
Andre Silva <andre.silva@appmax.org>
2023-08-06 11:34:27 -0300

    Adding the provider RepositoryLayerServiceProvider in the config/app.php file

--- config/app.php
+++ config/app.php
@@ -168,6 +168,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Custom Service Providers...
+         */
+        App\Providers\RepositoryLayerServiceProvider::class,
     ])->toArray(),
 
     /*
